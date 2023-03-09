dfcd99c6584ef7ac8de901b2aa34b4a8774fe595
jayrajpawar <122713715+jayrajpawar@users.noreply.github.com>
2023-03-09 13:58:32 +0530

    Initial commit

---
+++ Book_App/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApp.Models
+{
+    public class Book
+    {
+        [Key]
+        public int bookId { get; set; }
+        public string bookName { get; set; }
+        public string author { get; set; }
+        public int price { get; set; }
+    }
+}
